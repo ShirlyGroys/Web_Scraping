c37d5b2043307159c7f716f43ab3f7322c54ca53
shirly <shirlygroysberg@gmail.com>
2020-07-18 20:54:35 +0300

    initial cars app

---
+++ Cars24.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ('Welcome to cars app!')
+
+@app.route('/cars')
+def cars_view():
+    list_of_cars = []
+    for i in range(1, 6):           # First 5 pages
+        URL = 'https://www.cars24.com/buy-used-cars-chennai/?page={}'.format(i)
+        try:
+            page = requests.get(URL)
+        except Exception as e:
+            return ('An error occured: {}'.format(e))
+        results = BeautifulSoup(page.content, 'html.parser')
+        job_elems = results.find_all(itemprop="image")
+        for i in job_elems:
+            list_of_cars.append(i['alt'])
+            list_of_cars.append(str(i))
+    return '<br/>'.join(list_of_cars)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Hi!
+This is a web scraping app, that scrapes cars24.com and returns all posted cars pictures and their brand names.
